d8b3e8ee496348a88a657097227a07d147b2da10
samuel <samuelreynoso97@gmail.com>
2022-08-03 14:13:01 -0300

    first homework1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 23 ;
+const nuevoNum = 22;
 
 // Crea una variable booleana:
 const nuevoBool = true;
